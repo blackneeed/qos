e4016f79aac48a3bcdb87fce1e792537154aaf6b
Blackneeed <121721929+Blackneeed@users.noreply.github.com>
2024-07-26 22:07:48 +0200

    Mirror types from the std library as different names

--- src/types.h
+++ src/types.h
@@ -1,12 +1,13 @@
 #pragma once
+#include <stdint.h>
 
-typedef char i8;
-typedef unsigned char u8;
-typedef short i16;
-typedef unsigned short u16;
-typedef int i32;
-typedef unsigned int u32;
-typedef long long i64;
-typedef unsigned long long u64;
-typedef long long isize;
-typedef unsigned long long usize;
+typedef int8_t i8;
+typedef uint8_t u8;
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int64_t i64;
+typedef uint64_t u64;
+typedef size_t isize;
+typedef unsigned size_t usize;
