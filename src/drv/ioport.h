2f5c3cf3746008e2b210f6f40e8401f6f149e4a2
Blackneeed <121721929+Blackneeed@users.noreply.github.com>
2024-07-26 22:21:03 +0200

    No inline then

--- src/drv/ioport.h
+++ src/drv/ioport.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <types.h>
 
-static inline void io_outb(u16 port, u8 val);
-static inline u8 io_inb(u16 port);
-static inline void io_wait();
+void io_outb(u16 port, u8 val);
+u8 io_inb(u16 port);
+void io_wait();
