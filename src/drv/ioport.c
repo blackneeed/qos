485487e5445232b1fc33e587d1a3adc63c369ee4
Blackneeed <121721929+Blackneeed@users.noreply.github.com>
2024-07-26 22:17:11 +0200

    Oops this should be static and add static inline to the declarations in header of ioport

--- src/drv/ioport.c
+++ src/drv/ioport.c
@@ -1,18 +1,18 @@
 #include <types.h>
 
-inline void io_outb(u16 port, u8 val)
+static inline void io_outb(u16 port, u8 val)
 {
     __asm__ volatile ("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
 }
 
-inline u8 io_inb(u16 port)
+static inline u8 io_inb(u16 port)
 {
     u8 ret;
     __asm__ volatile ("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
     return ret;
 }
 
-inline void io_wait()
+static inline void io_wait()
 {
     io_outb(0x80, 0);
 }
